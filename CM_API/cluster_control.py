31c18304cbd53fd295103dac9269c3fa058736cc
Jamey Hanson <jphanso@evoforge.org>
2017-08-22 11:04:02 -0400

    added kms-acls client configuration

--- CM_API/cluster_control.py
+++ CM_API/cluster_control.py
@@ -4,6 +4,8 @@ Created on Aug 20, 2017
 @author: jamey
 
 Ref: https://cloudera.github.io/cm_api/docs/python-client/
+     https://cloudera.github.io/cm_api/epydoc/5.12.0/index.html
+     http://blog.cloudera.com/blog/2012/09/automating-your-cluster-with-cloudera-manager-api/
      https://github.com/cloudera/cm_api/tree/master/python
 '''
 
@@ -76,32 +78,41 @@ def main():
     cms = cm.get_service()
     
     # Toggle CM between start and stop
-    if cms.serviceState == 'STARTED':
-        controlCM(api, 'stop')
-    elif cms.serviceState == 'STOPPED':
-        controlCM(api, 'start')
-    else:
-        print(cms.serviceState)
-    
-#     # loop through the cluster and print details
-#     for clusters in api.get_all_clusters(view = "full"):
-#         my_clust = clusters
-#         print (my_clust.name + '\t' + 
-#                my_clust.version + '\t' + 
-#                my_clust.fullVersion)
-#          
-#     # start/stop the cluster
+#     if cms.serviceState == 'STARTED':
+#         controlCM(api, 'stop')
+#     elif cms.serviceState == 'STOPPED':
+#         controlCM(api, 'start')
+#     else:
+#         print(cms.serviceState)
+     
+    # loop through the all clusters 
+    for cluster in api.get_all_clusters(view = "full"):
+        print (cluster.name + '\t' + 
+               cluster.version + '\t' + 
+               cluster.fullVersion)
+        
+#         cluster.deploy_client_config()
+#         print ('Client config deployed')
+         
+        for service in cluster.get_all_services():
+            if service.type == 'KMS':
+                my_kms = service
+            
+                for kms_rcg in my_kms.get_all_role_config_groups():
+                    new_kms_acls = {'kms-acls.xml_role_safety_valve': '<property><name>hadoop.kms.acl.DELETE</name><value>fred fred</value></property>'}
+                    kms_rcg.update_config(new_kms_acls)
+                    print ('configuration updated')
+        # deploy cluster client configuration
+        cluster.deploy_cluster_client_config()
+        print ('deployed client configuration')
+         
+    # start/stop the cluster
 #     clust_cmd = my_clust.start()
 #     print ("Active: %s. Success: %s" % (clust_cmd.active, clust_cmd.success))
 #     clust_cmd.wait()
 #     print ("Active: %s. Success: %s" % (clust_cmd.active, clust_cmd.success))
-#      
-#      
-#      
-#      
-#     # list of running commands
-#     for cms_running_commands in cms.get_commands(view = None):
-#         print(cms_running_commands)
+     
+
     
 if __name__ == '__main__':
     main()
